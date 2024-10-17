a33d96ae42d26dcb25d83a3802764f61cb22c63f
eliasb03 <eliasbowmanschool@gmail.com>
2024-10-17 08:42:07 -0700

    calculated severity above threshold

--- scripts/06_temp-threshold_v1.R
+++ scripts/06_temp-threshold_v1.R
@@ -17,7 +17,8 @@ threshold_temperature <- 32
 # Calculates the amount of data above threshold
 time_above_threshold <- hobo_data %>%
   group_by(plot) %>%
-  summarize(total_time_above_threshold = sum(ifelse(ground_temp > threshold_temperature, 1, 0)))
+  summarize(total_time_above_threshold = sum(ifelse(ground_temp > threshold_temperature, 1, 0)))  %>%
+  mutate(treatment = substr(plot, 1, 3))
 
 ## Method 2 - Integral
 # Create a small dataframe, grouped by plot
@@ -27,5 +28,25 @@ severity_above_threshold <- hobo_data %>%
   mutate(above_threshold = ifelse(ground_temp > threshold_temperature, ground_temp - threshold_temperature, 0)) %>%
   mutate(time_interval = lead(date.time) - date.time) %>%
   mutate(area = above_threshold * as.numeric(time_interval)) %>%
-  summarize(heat_severity = sum(area, na.rm = TRUE)) 
+  summarize(heat_severity = sum(area, na.rm = TRUE)) %>%
+  mutate(treatment = substr(plot, 1, 3))
+
+## Visualizing temp severity between CON and OTC ####
+time_above_threshold %>% 
+  ggplot(aes(x = treatment, y = total_time_above_threshold)) +
+  geom_boxplot() +
+  geom_jitter(shape = 1, size = .5, width = .3) +
+  xlab("Treatment") +
+  ylab("time above") +
+  theme_bw()
+
+severity_above_threshold %>% 
+  ggplot(aes(x = treatment, y = heat_severity)) +
+  geom_boxplot() +
+  geom_jitter(shape = 1, size = .5, width = .3) +
+  xlab("Treatment") +
+  ylab("severity") +
+  theme_bw()
+
+
 
